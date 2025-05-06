01fa626cfcbce770a8a601d943caf9eef95bb303
Rapepong Pitijaroonpong <besutodesuka@gmail.com>
2025-05-06 14:25:47 +0700

    add embedding fintuning code (need mirate to Embedding model class)

--- model_registry/model_manager.py
+++ model_registry/model_manager.py
@@ -4,9 +4,9 @@ target model is BAAI/bge-m3 from hugging face laded with FlagEmbedding library
 https://github.com/FlagOpen/FlagEmbedding/tree/master/research/BGE_M3
 """
 from FlagEmbedding import BGEM3FlagModel
+import pandas as pd
 
-
-def finetune(model_name = 'BAAI/bge-m3', train_file):
+def finetune(model_name = 'BAAI/bge-m3', train_file=""):
     # load current model
     model = BGEM3FlagModel(model_name,
                        use_fp16=True)
@@ -15,13 +15,16 @@ def finetune(model_name = 'BAAI/bge-m3', train_file):
     # we can call package FlagEmbedding.finetune.embedder.encoder_only.m3 for finetune model
     return model
 
-def evaluate(model_name = 'BAAI/bge-m3', test_set):
+def evaluate(model_name = 'BAAI/bge-m3', test_set=""):
     # load model
     model = BGEM3FlagModel(model_name,
                        use_fp16=True)
     # load data
-    data = 
+    data = pd.read_csv(test_set)
+    data.dropna(["context"],axis = 1)
     # prediction
+    outputs = model.encode(sentences, return_dense=False, return_sparse=False, return_colbert_vecs=True)
+    colbert_vecs = outputs['colbert_vecs']
     # compare to label
     return score
 
