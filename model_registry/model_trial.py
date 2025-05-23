1ce64f07fd330847c1e559894872e8a2eb3c3b2f
Rapepong Pitijaroonpong <besutodesuka@gmail.com>
2025-05-24 02:26:27 +0700

    add model experimenting and model perfformance monitoring task to repository

--- model_registry/model_trial.py
+++ model_registry/model_trial.py
@@ -4,7 +4,7 @@ from modules.pipeline import run_experiment
 from modules.registry import get_current_model
 from modules.model import is_challenged
 
-def main():
+def trial():
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     client = mlflow.tracking.MlflowClient()
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
@@ -17,14 +17,14 @@ def main():
         model_name = "BAAI/bge-m3"
         init = True
     else:
-        model_name, _ = get_current_model(EXPERIMENT_NAME, "calinski_harabasz_score")
+        model_name, model_version , exp_info = get_current_model(EXPERIMENT_NAME, "calinski_harabasz_score")
     
     mlflow.set_experiment(EXPERIMENT_NAME)
     if is_challenged():
         version, score = run_experiment(init, experiment.name, model_name)
         print(f"Model run complete: version={version}, score={score}")
         # Add comparison logic here
+        return version, score
 
 if __name__ == "__main__":
-
-    main()
+    trial()
