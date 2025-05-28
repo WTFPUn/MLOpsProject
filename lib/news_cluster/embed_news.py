efa56c1fd6349f9c29930b84739a0c12273f0500
Rapepong Pitijaroonpong <besutodesuka@gmail.com>
2025-05-28 11:34:02 +0700

    add embedding model on api

--- lib/news_cluster/embed_news.py
+++ lib/news_cluster/embed_news.py
@@ -3,35 +3,54 @@ import pandas as pd
 from FlagEmbedding import BGEM3FlagModel
 import pickle
 import os
+import torch
+import io
 
+class Embeder:
+    def __init__(self, model_name = 'BAAI/bge-m3'):
+        self.model_name = model_name
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = BGEM3FlagModel(model_name, use_fp16=True, devices = device)
 
-def embed_colbert(input_csv_path, model_name = 'BAAI/bge-m3'):
-    print(f"📥 Reading: {input_csv_path}")
-    df = pd.read_csv(input_csv_path)
-    df = df.dropna(subset=["content"])
-    texts = df["content"].tolist()
+    def set_model(self, model_name):
+        self.model_name = model_name
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = BGEM3FlagModel(model_name, use_fp16=True, devices = device)
 
-    print(f"🧠 Loading model...")
-    model = BGEM3FlagModel(model_name, use_fp16=True)
+    def embed_colbert(self, input_csv_path, model_name = 'BAAI/bge-m3', api=True):
+        if api:
+            df = input_csv_path.copy()
+        else:
+            print(f"📥 Reading: {input_csv_path}")
+            df = pd.read_csv(input_csv_path)
+        df = df.dropna(subset=["content"])
+        texts = df["content"].tolist()
 
-    print(f"🔄 Embedding {len(texts)} texts using ColBERT vectors...")
-    outputs = model.encode(texts, return_dense=False, return_sparse=False, return_colbert_vecs=True)
-    colbert_vecs = outputs["colbert_vecs"]
+        if model_name != self.model_name:
+            print(f"🧠 Loading model...")
+            self.set_model(model_name)
+        else:
+            print(f"🧠 Cached model...")
+        
 
-    df["colbert_vecs"] = colbert_vecs
+        print(f"🔄 Embedding {len(texts)} texts using ColBERT vectors...")
+        outputs = self.model.encode(texts, return_dense=False, return_sparse=False, return_colbert_vecs=True)
+        colbert_vecs = outputs["colbert_vecs"]
 
-    # Create output filename
-    base_name = os.path.basename(input_csv_path).replace(".csv", "")
-    os.makedirs("embed_vector", exist_ok=True)
-    output_pkl_path = f"./embed_vector/{base_name}.pkl"
+        df["colbert_vecs"] = colbert_vecs
 
-    print(f"💾 Saving to: {output_pkl_path}")
-    with open(output_pkl_path, "wb") as f:
-        pickle.dump(df, f)
+        # Create output filename
+        base_name = os.path.basename(input_csv_path).replace(".csv", "")
+        os.makedirs("embed_vector", exist_ok=True)
+        output_pkl_path = f"./embed_vector/{base_name}.pkl"
 
-    print("✅ Done!")
+        print(f"💾 Saving to: {output_pkl_path}")
+        with open(output_pkl_path, "wb") as f:
+            pickle.dump(df, f)
 
-    return output_pkl_path
+        print("✅ Done!")
+
+        return output_pkl_path
 
 
 if __name__ == "__main__":
@@ -39,4 +58,5 @@ if __name__ == "__main__":
     parser.add_argument(default="input_csv", type=str, help="Path to the input CSV file (e.g., news_140525.csv)")
 
     args = parser.parse_args()
-    embed_colbert(args.input_csv)
+    test = Embeder()
+    test.embed_colbert(args.input_csv, api = False)
