6d9198bb791cdaf245f1801b5840951d19b76bd0
Rapepong Pitijaroonpong <besutodesuka@gmail.com>
2025-05-29 00:16:40 +0700

    finished and test model monitoring pipeline
    Co-authored-by: Punnawat Namwongsa <WTFPUn@users.noreply.github.com>

--- dags/model_monitor.py
+++ dags/model_monitor.py
@@ -5,6 +5,7 @@ from airflow.operators.python import PythonVirtualenvOperator
 from airflow import DAG
 from airflow.decorators import task
 from airflow.models import Variable
+
 import dotenv
 dotenv_path = dotenv.find_dotenv()
 if dotenv_path:
@@ -45,7 +46,7 @@ with DAG(
         try:
           print(f"Downloading file from S3: news_week_{date_parser(get_start_of_week(current_date))}.csv")
           s3.download_file("kmuttcpe393datamodelnewssum",
-          f"news_summary/news_week_{date_parser(get_start_of_week(current_date))}.csv",
+          f"data/news_week_{date_parser(get_start_of_week(current_date))}.csv",
           f"data/news_week_{date_parser(get_start_of_week(current_date))}.csv")
           csv_file = f"data/news_week_{date_parser(get_start_of_week(current_date))}.csv"
           print(f"✅ S3 file downloaded to: {csv_file}")
@@ -87,3 +88,28 @@ with DAG(
         requirements=["mlflow","pandas","numpy","scikit-learn==1.6.1"],
         python_version="3.10",
     )
+
+    def compare_model():
+        import sys
+        sys.path.append("/opt/airflow/lib")
+        sys.path.append("/opt/airflow/lib/model_registry")
+        from lib.model_registry.monitor_model import check_embedding
+        status = check_embedding()
+        print(f"should we finetune?: {status}")
+        # repo_name, _ , _= get_current_model("Vector_Prediction_Viz", "calinski_harabasz_score", "MINIMIZE")
+        # print(f"found {repo_name} on production")
+        # print(f"data is on : {csv_file}")
+        # return {
+        #     "csv_file": csv_file,
+        #     "repo_name": repo_name
+        # }
+
+    compare_model_task = PythonVirtualenvOperator(
+        task_id="model_comparison",
+        python_callable=compare_model,
+        # op_args=["{{ ti.xcom_pull(task_ids='load_test_monitor') }}"],
+        requirements=["mlflow","pandas"],
+        python_version="3.10",
+    )
+
+    load_data_task >> get_model_task >> compare_model_task
